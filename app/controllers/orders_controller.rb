df9acfb66ba44b9b0c80c72e19e48fb2268c5c53
Omer Shafique <omer.shafiqu3@gmail.com>
2025-01-23 01:58:55 +0500

    removed the retundent code of the orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,3 @@
-# flow is little complex, will work on making the code easy to understand.
-
 class OrdersController < ApplicationController
   include OrderFinder
   before_action :authenticate_user!
@@ -12,23 +10,4 @@ class OrdersController < ApplicationController
     @order = current_user.orders.find(params[:id])
   end
   
-  def checkout
-    stripe_token = params[:stripeToken]
-    order_params = {
-      product_id: params[:id],
-      name: params[:name],
-      email: params[:email],
-      address: params[:address],
-      phone: params[:phone]
-    }
-
-    service = StripeOperations::StripePaymentService.new(order_params, stripe_token)
-
-    begin
-      order = service.create_order_and_process_payment
-      redirect_to order_path(order), notice: "Order placed successfully!"
-    rescue StandardError => e
-      redirect_to checkout_product_path(Product.find(params[:id])), alert: "Payment failed: #{e.message}"
-    end
-  end
 end
